23c6a3ceb2db8155474446679c85f84180fb38e1
han-yan-ds <hanprogramming@gmail.com>
2020-01-30 10:00:07 -0700

    Added Div on top of Button to make it work on way more pages

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,8 @@
 const body = document.getElementsByTagName('body')[0];
-const openFormButton = document.createElement("button");
+const mainDiv = document.createElement('div');
+const openFormButton = document.createElement('button');
 
 openFormButton.id = "highlighter-extension-form-button";
 
-body.insertAdjacentElement("afterbegin", openFormButton);
+mainDiv.insertAdjacentElement('afterbegin', openFormButton);
+body.insertAdjacentElement('afterbegin', mainDiv);
