cc155ae2911f2479d9ec6ef1ea1c9973f67fd845
Han Yan <hanprogramming@gmail.com>
2020-01-29 19:27:58 -0700

    Modified executeScript so it runs a separate TS file instead

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,2 @@
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.executeScript({
-    "code": "var body = document.getElementsByTagName('body')[0];"+
-    "body.textContent = 'HELLO AND SALUTATIONS';"
-  })
-})
+var body = document.getElementsByTagName('body')[0];
+body.textContent = 'HELLO AND SALUTATIONS';
