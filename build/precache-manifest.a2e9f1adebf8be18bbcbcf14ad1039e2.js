8172cfda0af1575d1159a182054162ddcc31f2d2
han-yan-ds <hanprogramming@gmail.com>
2020-01-31 15:10:05 -0700

    Finally got npm run build to work

---
+++ build/precache-manifest.a2e9f1adebf8be18bbcbcf14ad1039e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053ba5d8185ba3e7014acbade50def1f",
+    "url": "/han-yan-ds/chrome-extension-exercise/index.html"
+  },
+  {
+    "revision": "6e31956890e4d68edd43",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f25bc093de8e34c09a88",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/js/2.253a98a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/js/2.253a98a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e31956890e4d68edd43",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/js/main.1ae7185e.chunk.js"
+  },
+  {
+    "revision": "99cd814623cb4ab502a5",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/js/runtime-main.513d5845.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/han-yan-ds/chrome-extension-exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/page/floatingButton.js
+++ src/page/floatingButton.js
@@ -6,15 +6,19 @@
 const body = document.getElementsByTagName('body')[0];
 const mainDiv = document.createElement('div');
 const openFormButton = document.createElement('button');
+const reactForm = document.createElement('div');
 
 mainDiv.classList.add('highlighter-extension-hide');
 mainDiv.id = "highlighter-extension-form-div";
 openFormButton.id = "highlighter-extension-form-button";
 
+ReactDOM.render(<App/>, reactForm);
+
 openFormButton.addEventListener('click', () => {
   console.log("HELLO WORLD");
-  // REPLACE WITH CODE THAT SHOWS/HIDES FORM
+  // toggle form class between HIDE
 })
 
 mainDiv.insertAdjacentElement('afterbegin', openFormButton);
+mainDiv.insertAdjacentElement('afterbegin', reactForm);
 body.insertAdjacentElement('afterbegin', mainDiv);
