5dc08e4048bdc076d1725ab5395f099ad9979761
han-yan-ds <hanprogramming@gmail.com>
2020-01-30 18:27:57 -0700

    Rewrote this so the button always exists, but we're toggling hide/unhide

---
+++ src/backgroundButtonClickResponse.js
@@ -0,0 +1,7 @@
+var mainDiV = document.getElementById("highlighter-extension-form-div");
+
+if (mainDiV.classList.contains('highlighter-extension-hide')) {
+  mainDiV.classList.remove('highlighter-extension-hide');
+} else {
+  mainDiV.classList.add('highlighter-extension-hide');
+}
