a5664f7d9ee8f9a0a755be58e27d0cca789acf12
han-yan-ds <hanprogramming@gmail.com>
2020-01-30 18:49:27 -0700

    Added Comments to Scripts

--- src/background/buttonClickResponse.js
+++ src/background/buttonClickResponse.js
@@ -1,3 +1,10 @@
+/**
+ * This script works in conjunction with the buttonClick.js
+ * to hide/unhide the extension upon clicking the button
+ * 
+ * However, this script is run on pages, not in the background
+ */
+
 var mainDiV = document.getElementById("highlighter-extension-form-div");
 
 if (mainDiV.classList.contains('highlighter-extension-hide')) {
